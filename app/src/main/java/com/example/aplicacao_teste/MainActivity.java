5653587da2354936826d95d3135efaf456050ce9
Vitor716 <62679174+Vitor716@users.noreply.github.com>
2020-07-09 09:19:56 -0300

    alguns codigos

--- app/src/main/java/com/example/aplicacao_teste/MainActivity.java
+++ app/src/main/java/com/example/aplicacao_teste/MainActivity.java
@@ -6,9 +6,24 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public final static String EXTRA_MESSAGE_NAME = "com.example.seguradora.MESSAGE";
+    public final static String EXTRA_MESSAGE_RG = "com.example.seguradora.RG";
+    public final static String EXTRA_MESSAGE_VALOR = "com.example.seguradora.VALUE";
+
+    private Spinner spinner;
+    String spinnerSelected;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+        public void Cadastrar(View v) {
+        EditText txtNome = findViewById(R.id.editText);
+        String nome = txtNome.getText().toString();
+        EditText RG = findViewById(R.id.editNumber);
+        String rg = RG.getText().toString();
+
 }
